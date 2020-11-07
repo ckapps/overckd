14d1e7d1a52505e5b65d55d8ca9b91e2e35e564c
christophka <chris@ckapp.net>
2020-11-07 10:49:08 +0100

    chore: setup lint-staged

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    // 'pre-commit': 'lint-staged',
+    'pre-commit':
+      'lerna run --concurrency 1 --stream precommit --since HEAD --exclude-dependents',
   },
 };
