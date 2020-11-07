17dcb06d8ccf1b6324dce96f0422ea60f67c476e
christophka <chris@ckapp.net>
2020-11-07 10:32:57 +0100

    chore: fix eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  /**
+   * Putting these in the root .eslintignore doesn't work
+   * because the eslint script is started for relative
+   */
+  ignorePatterns: [
+    // Distribution / output folder
+    'packages/*/dist',
+    // Generated files
+    '**/*.d.ts',
+    '**/*.js',
+  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
--- husky.config.js
+++ husky.config.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
