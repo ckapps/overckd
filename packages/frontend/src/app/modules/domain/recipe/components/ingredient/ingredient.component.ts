a1619573d771a05fa68f79878a4553449a93f0c7
christophka <chris@ckapp.net>
2020-11-15 11:39:22 +0100

    fix: use observable for showing ingredient

--- packages/frontend/src/app/modules/domain/recipe/components/ingredient/ingredient.component.ts
+++ packages/frontend/src/app/modules/domain/recipe/components/ingredient/ingredient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Ingredient } from '@overckd/domain';
 
@@ -8,21 +10,44 @@ import { Ingredient } from '@overckd/domain';
   styleUrls: ['./ingredient.component.scss'],
 })
 export class IngredientComponent implements OnInit {
+  /**
+   * The ingredient
+   */
   @Input() ingredient: Ingredient;
+
+  /**
+   * The ingredient
+   */
+  public ingredient$: Observable<Ingredient>;
+  /**
+   * Alternatives for the ingredient
+   */
+  public alternatives$: Observable<string>;
+
+  private passedIngredient$: BehaviorSubject<Ingredient>;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.passedIngredient$ = new BehaviorSubject(this.ingredient);
+    this.ingredient$ = this.passedIngredient$.asObservable();
 
-  get alternatives() {
-    return this.ingredient.alternatives.reduce((acc, cur, i) => {
-      let separator = '';
+    // Set up alternatives$
+    this.alternatives$ = this.passedIngredient$.pipe(
+      map(({ alternatives }) =>
+        alternatives.length === 0
+          ? null
+          : alternatives.reduce((acc, cur, i) => {
+              let separator = '';
 
-      if (i > 0) {
-        separator =
-          i + 1 < this.ingredient.alternatives.length ? ', ' : ' oder ';
-      }
+              if (i > 0) {
+                separator =
+                  i + 1 < this.ingredient.alternatives.length ? ', ' : ' oder ';
+              }
 
-      return `${acc}${separator}${cur}`;
-    }, '');
+              return `${acc}${separator}${cur}`;
+            }, ''),
+      ),
+    );
   }
 }
