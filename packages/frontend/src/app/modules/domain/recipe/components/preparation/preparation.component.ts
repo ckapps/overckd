2a2892c71c6313974ef42c1b6e45e98e6676531e
christophka <chris@ckapp.net>
2020-11-16 00:51:14 +0100

    feat: display ingredients and preparation steps for recipe groups

--- packages/frontend/src/app/modules/domain/recipe/components/preparation/preparation.component.ts
+++ packages/frontend/src/app/modules/domain/recipe/components/preparation/preparation.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '@overckd/domain';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+interface PreparationGroup {
+  label?: string;
+  steps: Recipe['steps'];
+  stepsEnumerated?: Recipe['stepsEnumerated'];
+  start?: number;
+}
+
+/**
+ * Component for displaying recipe preparation steps
+ */
 @Component({
   selector: 'overckd-preparation',
   templateUrl: './preparation.component.html',
@@ -10,15 +22,59 @@ import { Recipe } from '@overckd/domain';
 export class PreparationComponent implements OnInit {
   @Input() recipe: Recipe;
 
+  private recipe$: BehaviorSubject<Recipe>;
+  public preparationGroups$: Observable<PreparationGroup[]>;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipe$ = new BehaviorSubject<Recipe>(this.recipe);
 
-  public get steps() {
-    return this.recipe.steps;
+    this.preparationGroups$ = this.recipe$
+      .asObservable()
+      .pipe(map(recipe => this.getPreparationGroups(recipe)));
   }
 
-  public get asList() {
-    return Array.isArray(this.steps);
+  public isArray(obj: any) {
+    return Array.isArray(obj);
+  }
+
+  private getPreparationGroups(recipe: Recipe): PreparationGroup[] {
+    const { groups } = recipe;
+
+    if (!groups || groups.length === 0) {
+      return [
+        {
+          steps: recipe.steps,
+          stepsEnumerated: recipe.stepsEnumerated,
+        },
+      ];
+    }
+
+    let start = 1;
+    const stepsFromGroups = groups.map<PreparationGroup>(
+      ({ label, steps, stepsEnumerated }) => {
+        const group = {
+          start,
+          label,
+          steps,
+          stepsEnumerated,
+        };
+
+        start += this.isArray(steps) ? steps.length : 1;
+
+        return group;
+      },
+    );
+
+    return [
+      ...stepsFromGroups,
+      {
+        label: 'Weitere Zubereitung',
+        start,
+        steps: recipe.steps,
+        stepsEnumerated: recipe.stepsEnumerated,
+      },
+    ];
   }
 }
