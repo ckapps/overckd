44b430078bdaef97734607869d7b69c1bfdd2989
christophka <chris@ckapp.net>
2020-11-15 12:53:18 +0100

    feat: add scale factor to ingredient

--- packages/domain/src/ingredient.ts
+++ packages/domain/src/ingredient.ts
@@ -3,6 +3,17 @@ export interface Ingredient {
   unit?: string;
   name: string;
 
+  /**
+   * Optional scale factor for this ingredient, to control by how much this ingredient
+   * scales, when the overall recipe is scaled.
+   * Only applies, if `amount` is a `number`.
+   *
+   * Default is `1`.
+   */
+  scaleFactor?: number;
+  /**
+   * Wheter this ingredient can be considered optional.
+   */
   optional?: boolean;
   alternatives?: string[];
 }
