4bbd257261e4fb942060e3e377428d0f127cfe87
christophka <chris@ckapp.net>
2021-04-23 00:09:16 +0200

    feat(server): add pagination utility operators

---
+++ packages/server/src/core/search/transform-from-request-property.operator.ts
@@ -0,0 +1,36 @@
+import { DataQuery } from '@overckd/domain/dist/search';
+import { unflattenQuery } from '@overckd/domain-rx/dist/search';
+import { OperatorFunction } from 'rxjs';
+import { pluck, map } from 'rxjs/operators';
+
+import { mapPaginationTyped, FromQueryString } from '../pagination';
+
+import { HttpRequest } from '@marblejs/core';
+
+/**
+ * Utility function for transform from request properties
+ *
+ * @param property Property to extract information from
+ *
+ * @returns
+ */
+export function transformFromRequestProperty<
+  TBody = unknown,
+  TParams = unknown,
+  TQuery extends Record<string, unknown> = Record<string, unknown>
+>(
+  property: 'query',
+): OperatorFunction<
+  HttpRequest<TBody, TParams, FromQueryString<TQuery>>,
+  DataQuery<TQuery>
+> {
+  return obs$ =>
+    obs$.pipe(
+      // Extract the requested property
+      pluck(property),
+      // Map pagination information
+      mapPaginationTyped(),
+      // Unwrap
+      map(unflattenQuery),
+    );
+}
