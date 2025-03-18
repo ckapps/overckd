eaa98f3a7a43a663cd91b6b5e57d3c4f9760ddc3
Chris <chris@ckapp.net>
2025-03-18 21:11:02 +0100

    feat(recipe-infra-rxdb): extract into lib

--- apps/desktop/src/app/legacy/db/collections/db.collections.create.ts
+++ apps/desktop/src/app/legacy/db/collections/db.collections.create.ts
@@ -1,4 +1,5 @@
 import { createRecipeCollectionDbCollection } from '@_shared/recipe-collection/infra-rxdb';
+import { createRecipeDbCollection } from '@_shared/recipe/infra-rxdb';
 import { createTagDbCollection } from '@_shared/tag/infra-rxdb';
 import {
   logEnterExit,
@@ -20,7 +21,6 @@ import {
   TagDbCollectionToken,
 } from './db.collections.tokens';
 import { createIngredientDbCollection } from './ingredients/ingredient.db.create';
-import { createRecipeDbCollection } from './recipe/recipe.db.create';
 
 type DbCollectionDependencies = BoundDependency<unknown, ContextDependency>[];
 
