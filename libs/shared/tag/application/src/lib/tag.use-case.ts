df34b6ea79abf3f2987fe0b0cd576b5d42da9240
Chris <chris@ckapp.net>
2025-03-25 10:14:33 +0100

    refactor(tag-application): use service for tag use cases

--- libs/shared/tag/application/src/lib/tag.use-case.ts
+++ libs/shared/tag/application/src/lib/tag.use-case.ts
@@ -1,13 +1,21 @@
 import { TagId } from '@overckd/domain-experimental';
-import { Effect } from 'effect';
+import { Effect, pipe } from 'effect';
 import { TagRepo } from './tag.repostitory';
 
-/**
- * Find a tag by its ID.
- * @param id Tag ID
- */
-export const findById = (id: TagId) =>
-  Effect.gen(function* () {
+export class TagUseCase extends Effect.Service<TagUseCase>()('TagUseCase', {
+  effect: Effect.gen(function* () {
     const repo = yield* TagRepo;
-    return yield* repo.findById(id);
-  }).pipe(Effect.withSpan('Tag.findById', { attributes: { id } }));
+
+    /**
+     * Find a tag by its ID.
+     * @param id Tag ID
+     */
+    const findById = (id: TagId) =>
+      pipe(
+        repo.findById(id),
+        Effect.withSpan('Tag.findById', { attributes: { id } }),
+      );
+
+    return { findById } as const;
+  }),
+}) {}
